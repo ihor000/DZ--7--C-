edf770483ec0e4dee32f486d4dc442f7e78b0326
ihor000 <ihor.silnov@yandex.ru>
2022-12-25 20:28:36 +0300

    text

---
+++ 1/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(double[,] array)                                          
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble();
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 3;
+int n = 4;
+double[,] array = new double [m, n];
+
+FillArray(array);
+PrintArray(array);
